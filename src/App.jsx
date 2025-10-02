218ec9200d94b094032ee7bdd715bed51bcfa2a8
medogma1 <borgnasr@gmail.com>
2025-10-02 10:59:13 +0300

    Fix React Router basename for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import './App.css'
 
 function App() {
   return (
-    <Router>
+    <Router basename="/newreactproject">
       <div className="app">
         <Header />
         <main className="main-content">
